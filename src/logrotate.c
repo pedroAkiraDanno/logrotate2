eb50d6c03f6650cf14a117f73ccc9f3a3b8fa17f
PedroAkira <pedro-akira@hotmail.com>
2022-07-02 17:26:42 -0300

    feat(print_copyright) add function print_copyright() and call with version print_copyright,--print_copyright

--- src/logrotate.c
+++ src/logrotate.c
@@ -57,7 +57,7 @@ static unsigned long MAX_SIZE = 0;
   void process(void);
   void help();
   void version();
-
+  void print_copyright();
 
 
 
@@ -241,8 +241,33 @@ process(void)
 
 
  void version(){
-    printf("Version: v2.0");    
+    printf("Version: v2.0\n\n");    
  }
+
+
+
+ void print_copyright(void){
+
+  puts("Licensed under the Apache License, Version 2.0, the 'License'\n"
+   "you may not use this file except in compliance with the License.\n"
+   "You may obtain a copy of the License at\n\n"
+ 
+   "http://www.apache.org/licenses/LICENSE-2.0\n\n"
+ 
+   "Unless required by applicable law or agreed to in writing, software\n"
+   "distributed under the License is distributed on an AS IS BASIS,\n"
+   "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+   "See the License for the specific language governing permissions and\n"
+   "limitations under the License.\n\n\n"
+ 
+ 
+   "02-07-2022, v2.0, Written by Pedro Akira Danno Lima\n\n\n\n");
+  
+ }
+
+
+
+
 
 
 
@@ -265,6 +290,12 @@ main(int argc, char *argv[])
         exit(EXIT_SUCCESS);                
     }
 
+
+    //to print_copyright(void)
+    if (strcmp(argv[1], "copyright" ) == 0 || strcmp(argv[1], "--copyright" ) == 0){
+        print_copyright();
+        exit(EXIT_SUCCESS);                
+    }
 
 
 
