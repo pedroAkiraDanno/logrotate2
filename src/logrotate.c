9f5089fdc347edf61c67e8c7f15c9a29725931fe
PedroAkira <pedro-akira@hotmail.com>
2022-07-02 13:09:31 -0300

    fix(help): fix function help() in if command

--- src/logrotate.c
+++ src/logrotate.c
@@ -251,7 +251,7 @@ main(int argc, char *argv[])
 
 
     //to help(void)
-    if (strcmp(argv[1], "help" || strcmp(argv[1], "--help" || strcmp(argv[1], "-h") == 0){
+    if (strcmp(argv[1], "help" ) == 0 || strcmp(argv[1], "--help" ) == 0 || strcmp(argv[1], "-h" ) == 0 ){
         help();
         exit(EXIT_SUCCESS);                
     }
